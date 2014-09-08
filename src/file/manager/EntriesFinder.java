031fc622e0f272e82aae2ab80de764cc197bd14f
Dennis <Dennis@LaptopDennis>
2014-09-08 17:40:54 +0200

    New entries finder

--- src/file/manager/EntriesFinder.java
+++ src/file/manager/EntriesFinder.java
@@ -6,12 +6,21 @@
 
 package file.manager;
 
+import java.nio.file.Paths;
+
 /**
  *
  * @author Dennis
  */
 public class EntriesFinder {
     static String simpleBasePath(){
+        
+        String currPath = Paths.get("").toAbsolutePath().toString();
+        
+        if(!currPath.isEmpty()){
+            return currPath + "\\files\\RaafEntries";
+        }
+        
         String usrDomain = System.getenv("USERDOMAIN");
         if (usrDomain != null && usrDomain.equalsIgnoreCase("LaptopDennis")) {
             return "C:\\Users\\Dennis\\Dropbox\\RaafEntries";
