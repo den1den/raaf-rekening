c387ea5774e618ab54a7e57b9edea661fc1c98c5
Dennis <Dennis@192.168.1.66>
2014-09-09 00:41:56 +0200

    unsoprted quit

---
+++ RUN 16.bat
@@ -0,0 +1,18 @@
+pause
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+start java -jar "RaafBeheer.jar" testing
+pause
